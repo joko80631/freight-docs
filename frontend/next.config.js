1f8eb3355af9f3a71102c6bd935686457d69236f
joko80631 <bagdonaviciusbm@gmail.com>
2025-03-30 13:06:05 +0300

    Update: frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,6 +9,10 @@ const nextConfig = {
   },
   experimental: {
     optimizeCss: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
   }
 }
 
