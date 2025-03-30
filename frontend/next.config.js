6f028e6775972c23467651f7b161bb0a3e6390e9
joko80631 <bagdonaviciusbm@gmail.com>
2025-03-30 12:55:06 +0300

    Update: frontend/.babelrc frontend/next.config.js frontend/package-lock.json frontend/package.json frontend/vercel.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [],
+  },
+  experimental: {
+    optimizeCss: true,
+  }
 }
 
 module.exports = nextConfig 
