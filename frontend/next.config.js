b6d0f55e0026c2febdcee551c891390067ec31cb
joko80631 <bagdonaviciusbm@gmail.com>
2025-03-30 20:28:10 +0300

    Update: frontend/.eslintrc.json frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
